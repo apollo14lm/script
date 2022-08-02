aa7f0311886fdcce04072d3306e8f643b2e89aae
apollo14lm <99069997+apollo14lm@users.noreply.github.com>
2022-08-02 20:40:02 +0530

    Delete git-config.sh

--- git-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# CodeCommit passwordless access using IAM role
-git config --global credential.helper '!aws codecommit credential-helper $@' 
-git config --global credential.UseHttpPath true
